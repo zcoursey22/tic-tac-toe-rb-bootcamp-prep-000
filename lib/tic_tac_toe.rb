1f6f9169126f1f413e35bd00c47eec74bfb698e7
Zachary Coursey <zcoursey22@gmail.com>
2018-02-12 19:40:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,52 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def input_to_index(input)
+  return input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if (index >= 0 && index < 9 && position_taken?(board, index) == false)
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == '' || !board[index]
+    false
+  else
+    true
+  end
+end
+
+def move(board, input, char = "X")
+  board[input] = char
+  return board
+end
+
+def turn(board)
+  puts 'Please enter 1-9:'
+  input = gets
+  index = input_to_index(input)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  moveCount = 0
+  while moveCount < 9
+    turn(board)
+    moveCount += 1
+  end
+end
