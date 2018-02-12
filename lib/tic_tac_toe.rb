3d5e28d8a8414ffa034b1b6ef86d285e78a14fdd
Zachary Coursey <zcoursey22@gmail.com>
2018-02-12 19:40:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,3 +50,21 @@ def play(board)
     moveCount += 1
   end
 end
+
+def turn_count(board)
+  spacesUsed = 0
+  board.each { |space| 
+    if space != ' ' && space != ''
+      spacesUsed += 1
+    end
+  }
+  spacesUsed
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    'X'
+  else
+    'O'
+  end
+end
