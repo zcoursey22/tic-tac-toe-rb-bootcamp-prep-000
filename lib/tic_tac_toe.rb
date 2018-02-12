90e44f5bc6096f577a54b4bf89ccc232df810e6a
Zachary Coursey <zcoursey22@gmail.com>
2018-02-12 19:41:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -66,5 +68,49 @@ def current_player(board)
     'X'
   else
     'O'
+  end
+end
+
+def won?(board)
+  xWon = false
+  oWon = false
+  WIN_COMBINATIONS.each { |winCombo|
+    xWon = winCombo.all? do |index|
+      board[index] == 'X'
+    end
+    if xWon == true
+      return winCombo
+    end
+    oWon = winCombo.all? do |index|
+      board[index] == 'O'
+    end
+    if oWon == true
+      return winCombo
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.none? { |index| index == ' ' || index == ''}
+end
+
+def draw?(board)
+  if full?(board) == true && won?(board) == false
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  full?(board) || won?(board)
+end
+
+def winner(board)
+  if won?(board) != false
+    board[won?(board)[0]]
+  else
+    nil
   end
 end
